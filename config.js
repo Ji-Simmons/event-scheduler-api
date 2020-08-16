17df81f756c2c867e8e2ddb12a585196f1e000a8
Ji Simmons <ji.simmons@yahoo.com>
2020-08-15 20:34:15 -0600

    fixing routes

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const DBNAME = 'tododb';
 module.exports= {
    // remove process when localhost, add when live.
-   PORT: /* process.env.PORT || */ 5556,
+   PORT: process.env.PORT || 5556,
    DBNAME,
    APPNAME: 'Calendar',
    DBURI: `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@blog-z710y.mongodb.net/${DBNAME}?retryWrites=true&w=majority`,  
